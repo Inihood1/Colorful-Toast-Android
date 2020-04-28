9aaaa63db64847c93e3503dda7d3cc2b8c4d192d
INI HOOD <inihood@gmail.com>
2020-04-28 08:47:55 +0100

    A simple in Android

---
+++ app/src/main/java/com/inihood/colorfultoastsample/MainActivity.java
@@ -0,0 +1,35 @@
+package com.inihood.colorfultoastsample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.Gravity;
+
+import com.inihood.colorfultoast.ColorfulToast;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        String sms = "Hello My Awesome Toast";
+
+        new ColorfulToast.Builder(this)
+                .setText(sms)
+                .setStroke(2, Color.BLUE)
+                .setBackgroundColor(Color.GRAY)
+                .setToastPosition(Gravity.BOTTOM)
+                .setTextColor(Color.BLACK)
+                .setTextBold()
+               // .font(R.font.dosis)
+              //  .iconStart(getIcon())
+               // .iconEnd(getIcon())
+                .setCornerRadius(5)
+                .setTextSize(18)
+                .show();
+
+
+    }
+}
